aba8ceee52d83ad0336c9bc53b16eba8ac7098e1
Camille Noufi <camillenoufi@DN0a1f89a6.SUNet>
2018-10-08 03:56:54 -0700

    fft spectrum and moon are working!

--- host/VisualSine.cpp
+++ host/VisualSine.cpp
@@ -370,7 +370,106 @@ void idleFunc( )
     glutPostRedisplay( );
 }
 
+//-----------------------------------------------------------------------------
+// Name: getIntersect( )
+// Desc: draw time domain wave form and attach to mountain peak
+//-----------------------------------------------------------------------------
+GLfloat getIntersect(GLfloat y, GLfloat x1,GLfloat y1,GLfloat x2,GLfloat y2){
+    GLfloat m = (y2-y1)/(x2-x1); //slope
+    return((y-y1+m*x1)/m); //rearranged point slope form
+}
 
+//-----------------------------------------------------------------------------
+// Name: drawSnowCap( )
+// Desc: draw time domain wave form and attach to mountain peak
+//-----------------------------------------------------------------------------
+GLfloat bolt_rot = 1;
+void drawSnowCap(GLfloat y_snow_min, GLfloat x1,GLfloat y1,GLfloat x2,GLfloat y2,GLfloat x3,GLfloat y3,GLfloat lw) {
+
+    GLfloat xL = -1;
+    GLfloat xR = -1;
+    GLfloat x = -1;
+    GLfloat inc = 1/g_bufferSize;
+    GLfloat y_snow = -1;
+    GLint nWaves = 6;
+
+    for(int j=0; j<nWaves; j++) {
+        y_snow = y_snow_min + ((GLfloat)j/nWaves)*(y2-y_snow);
+        xL = getIntersect( y_snow , x1 , y1 , x2 , y2);     // starting point
+        xR = getIntersect( y_snow , x2 , y2 , x3 , y3);      //ending point
+        inc = ::fabs( (xR-xL) / g_bufferSize);      // increment
+        x = xL;
+        glLineWidth( lw );  // time domain line width
+        glColor3f( 1.0, 1.0, 1.0 ); // color
+        // apply the transform window
+        //apply_window( (float*)g_buffer, g_window, g_bufferSize );
+        // start primitive
+        glBegin( GL_LINE_STRIP );
+            // loop over buffer
+            for(int i = 0; i < g_bufferSize; i++ )
+            {
+                if (::fabs(g_buffer[i])>=1.5) {
+
+                    glColor3f(1.0,1.0,i/g_bufferSize); //yellow
+                    //do i need to do this again??
+                    glRotatef(bolt_rot,0,1,0); bolt_rot += 1;
+                    glBegin( GL_LINE_STRIP );
+                    glVertex2f( -2, -0.5*::fabs(g_buffer[i]+y_snow));
+                    //CHANGE BACKGROUND COLOR TO WHITE
+                }
+                else {
+                    glVertex2f( x, 0.2*g_buffer[i]+y_snow);
+                }
+
+                // plot
+                // increment x
+                x += inc;
+            }
+            // end primitive
+        glEnd();
+    }
+}
+
+//-----------------------------------------------------------------------------
+// Name: drawMountain( )
+// Desc: draw static mountain with given vertices
+//-----------------------------------------------------------------------------
+void drawMountain(GLfloat lw, GLfloat mx, GLfloat mb, GLfloat mh, GLfloat w, GLfloat y_off) {
+
+    GLfloat x1 = mx-w, y1 = mb;
+    GLfloat x2 = mx, y2 = mh+y_off;
+    GLfloat x3 = mx+w, y3 = mb;
+    GLfloat y_snow = (mh+y_off) - (mh+y_off-mb)/4;
+
+    glLineWidth( lw ); //line width
+
+    glBegin(GL_LINE_STRIP);
+    {
+        glColor3f( 0.0 , 0.05, 0.1); //aged blue green
+        glVertex3f( x1,y1,0.1 );
+        glColor3f( 1.0 , 1.0, 1.0); //white
+        glVertex3f( x2,y2,0 );
+        glColor3f( 0.0 , 0.05, 0.1); //aged blue green
+        glVertex3f( x3,y3,-0.1 );
+    }
+    glEnd();
+
+    drawSnowCap(y_snow,x1,y1,x2,y2,x3,y3,lw/16);
+}
+
+void drawMoon(float radius)
+{
+    const float DEG2RAD = MY_PIE/180;
+    glBegin(GL_LINE_LOOP);
+
+    for (int i=0; i<360; i++)
+    {
+      float degInRad = i*DEG2RAD;
+      glVertex2f(cos(degInRad)*radius+4.4,sin(degInRad)*radius+3.3);
+    }
+
+    glEnd();
+}
 
 //-----------------------------------------------------------------------------
 // Name: displayFunc( )
@@ -380,97 +479,63 @@ void displayFunc( )
 {
     // local state
         static GLfloat zrot = 0.0f, c = 0.0f;
+        static GLfloat mb = 0.0, mh = 3.0; //mountain base y, mountain height y
+        static GLfloat mx1 = -4.0, mx2 = -2.5, mx3=-0.2, mx4=1.75, mx5 = 4.0, mx6 = 5.7;//mountain x vertices
 
         // clear the color and depth buffers
         glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
         // save current matrix state
-//        glPushMatrix( );
+        glPushMatrix( );
 
-/******** TIME DOMAIN DISPLAY *******/
-//            glPushMatrix( );
-                // line width
+           glPushMatrix( );
+               SAMPLE * buffer[g_bufferSize];
+               // get the latest (possibly preview) window
+               memset( buffer, 0, SND_FFT_SIZE * sizeof(SAMPLE) );
+               // copy currently playing audio into buffer
+               memcpy( buffer, g_buffer, g_bufferSize * sizeof(SAMPLE) );
 
-/******* DRAW STATIC MOUNTAINS *****************/
+                /******* DRAW STATIC MOUNTAINS & TIME DOMAIN *****************/
                 glPushMatrix();
-                    glLineWidth( 2.5 );
-                    glColor3f( 18/255 , 95/255, 83/255); //aged blue green
-                    glBegin(GL_LINE_STRIP);
-                    {
-                        glVertex2f( -5, -5);
-                        glVertex2f( 2, 4);
-                    }
-                    glEnd();
-                glPop();
+                    drawMountain(8.0, mx1,mb,mh,2.5,0.0);
+                    drawMountain(12.0, mx2,mb,mh,2.5,-0.3);
+                    drawMountain(18.0, mx3,mb-0.1,mh,3.0,0.8);
+                    drawMountain(9.0, mx4,mb-0.15,mh,3.5,0.3);
+                    drawMountain(15.0, mx5,mb-0.25,mh,3.0,-0.6);
+                    drawMountain(30.0, mx6,mb-0.5,mh,2.0,-0.2);
+                    drawMoon(0.4);
+                glPopMatrix();
 
-/****** VARIABLES for time/fft signals  ***/
-                // apply the transform window
-//                apply_window( (float*)g_buffer, g_window, g_bufferSize );
-
-                // define a starting point
-                GLfloat x = -5;
-                // increment
-                GLfloat xinc = ::fabs(x*2 / g_bufferSize);
-                // time domain line width
-                glLineWidth( 0.5 );
-                // color
-                glColor3f( 1.0, 1.0, 1.0 );
-
-                // start primitive
-                glBegin( GL_LINES );
-                    // loop over buffer
-                    for( int i = 0; i < g_bufferSize; i++ )
-                    {
-                        if (::fabs(g_buffer[i]>=1.5)) {
-                            glColor3f(1.0,1.0,0);
-                            //do i need to do this again??
-                            glBegin( GL_LINE_STRIP );
-                            glVertex2f( x, 3*g_buffer[i]-2);
-                            //CHANGE BACKGROUND COLOR TO WHITE
-                        }
-                        else {
-                            glVertex2f( x, 0.7*g_buffer[i]-2);
-                        }
-
-                        // plot
-                        // increment x
-                        x += xinc;
-                    }
-                    // end primitive
-                glEnd();
-
-                SAMPLE * buffer[g_bufferSize];
+                /******* DRAW FFT *****************/
                 // reset starting point
-                x = -5;
-                // get the latest (possibly preview) window
-                memset( buffer, 0, SND_FFT_SIZE * sizeof(SAMPLE) );
-
-                // copy currently playing audio into buffer
-                memcpy( buffer, g_buffer, g_bufferSize * sizeof(SAMPLE) );
-
+                GLfloat x_anchor = -7.0f;
+                GLfloat xinc = ::fabs(4*x_anchor / g_bufferSize);
+                GLfloat depth = 255;
                 // take forward FFT; result in buffer as FFT_SIZE/2 complex values
                 rfft( (float *)buffer, g_fft_size/2, FFT_FORWARD );
                 // cast to complex
                 complex * cbuf = (complex *)buffer;
-
-                // time domain line width
-                glLineWidth( 1.0 );
                 // color
-                glColor3f( 0.0, 1.0, 0.0 );
-                glBegin( GL_LINE_STRIP );
+
                     // loop over buffer
-                    for( int i = 0; i < g_bufferSize/2; i++ )
-                    {
-                        glVertex2f( x, 10*cmp_abs(cbuf[i]) + 2 );
-                        x += 2*xinc;
+                    for(int j=0; j<depth; j++){
+                        GLfloat j_fl = (GLfloat)j;
+                        GLfloat x = x_anchor;
+                        glLineWidth( 0.5+1.5*j_fl/depth);
+                        glBegin( GL_LINE_STRIP );
+                        for(int i = 0; i < g_bufferSize/2; i++ )
+                        {
+                            glColor3f( 0.0, (255/depth)*j_fl/255, 1-(float)i/2/g_bufferSize );
+                            glVertex2f( x, 8*(1+5*j_fl/depth)*cmp_abs(cbuf[i])-(5*j_fl/depth));
+                            x += 0.1*xinc+6*log(1+j_fl/depth)*xinc;
+                        }
+                        glEnd();
                     }
                     // end primitive
-                glEnd();
-//            glPopMatrix();
-/*********   FFT SPECTRUM PART  *********/
+            glPopMatrix();
 
 
-//        glPopMatrix();
+        glPopMatrix();
 
         // flush!
         glFlush( );
